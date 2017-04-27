fed74c2e6238cbd7f43f6ac18a7502a873fd0f2a
Robert Johnson <mtelbertbiker@gmail.com>
2017-04-26 21:05:51 -0600

    Crude Rental Item Detail Working with Form.  Selected / Available FeeGroups working.  Issue with Rental Type.  WIP.

--- src/app/posconfig/rentals/rental-item-detail/rental-item-detail.component.ts
+++ src/app/posconfig/rentals/rental-item-detail/rental-item-detail.component.ts
@@ -4,6 +4,10 @@ import {VenueService} from '../../../venues/venue.service';
 import {ActivatedRoute, Params} from '@angular/router';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Venue} from '../../../shared/pos-models/venue.model';
+import {forEach} from '@angular/router/src/utils/collection';
+import {FeeGroup} from '../../../shared/pos-models/fee-group.model';
+import {RentalItemFeeGroup} from '../../../shared/pos-models/rental-item-fee-group.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-rental-item-detail',
@@ -15,6 +19,7 @@ export class RentalItemDetailComponent implements OnInit {
   rentalItemDetailForm: FormGroup;
   vid: number;
   index: number;
+  subscription: Subscription;
 
   constructor(private venueService: VenueService,
               private route: ActivatedRoute) { }
@@ -29,17 +34,20 @@ export class RentalItemDetailComponent implements OnInit {
           this.index =  +valpair[1];
           this.venue = this.venueService.getVenue(this.vid);
           this.rentalItem = this.venue.RentalItems[this.index];
+          this.initForm();
+          this.subscription = this.rentalItemDetailForm.valueChanges.subscribe(
+            (value) => this.updateRentalItem(this.rentalItemDetailForm.value)
+          );
         }
       );
-    this.initForm();
   }
   private initForm() {
     const Name = this.rentalItem.Name;
-    const RentalType = this.rentalItem.RentalTypeId;
+    const RentalTypeId = this.rentalItem.RentalTypeId;
     this.rentalItemDetailForm = new FormGroup(
       {
         'Name': new FormControl(Name, Validators.required),
-        'RentalType': new FormControl(RentalType, Validators.required),
+        'RentalTypeId': new FormControl(RentalTypeId, Validators.required),
       }
     );
   }
@@ -53,9 +61,44 @@ export class RentalItemDetailComponent implements OnInit {
     }
   }
 
-  onAddFeeGroup() {}
+  onAddFeeGroup(fgId: number) {
+    this.venue.FeeGroups.forEach((feeGroup) => {
+      if (feeGroup.FGId === fgId) {
+        const rentalFeeGroup = new RentalItemFeeGroup(this.rentalItem.RId, fgId, this.venue.lId);
+        this.rentalItem.RentalItemFeeGroups.push(rentalFeeGroup);
+      }
+    });
+  }
 
-  onDeleteFeeGroup(index: number) {}
+  onDeleteFeeGroup(fgId: number) {
+    let i = 0;
+    for (i = 0; i < this.rentalItem.RentalItemFeeGroups.length; i++) {
+      if (this.rentalItem.RentalItemFeeGroups[i].FGId === fgId) {
+        this.rentalItem.RentalItemFeeGroups.splice(i, 1);
+        return;
+      }
+    }
+  }
 
   onSubmit() {}
+
+  getAvailableFeeGroups() {
+    const availableFeeGroups = [];
+    this.venue.FeeGroups.forEach((feeGroup) => {
+      let found = false;
+      this.rentalItem.RentalItemFeeGroups.forEach((rentalFeeGroup) => {
+        if (feeGroup.FGId === rentalFeeGroup.FGId) {
+          found = true;
+        }
+      });
+      if (!found) {
+        availableFeeGroups.push(feeGroup);
+      }
+    });
+    return availableFeeGroups;
+  }
+  updateRentalItem(newRentalItem: RentalItem) {
+    this.rentalItem.Name = newRentalItem.Name;
+    this.rentalItem.RentalTypeId = newRentalItem.RentalTypeId;
+  }
 }
