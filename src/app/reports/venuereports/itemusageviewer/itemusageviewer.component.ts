29b774a112c98e6b718b8763037451b2dee4a8e9
Robert Johnson <mtelbertbiker@gmail.com>
2018-05-18 17:25:30 -0600

    Working report.  Still needs work.

---
+++ src/app/reports/venuereports/itemusageviewer/itemusageviewer.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import {Venue} from '../../../shared/pos-models/venue.model';
+import {VenueService} from '../../../venues/venue.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-itemusageviewer',
+  templateUrl: './itemusageviewer.component.html'
+})
+export class ItemusageviewerComponent implements OnInit {
+  venue: Venue;
+  id: number;
+  viewerContainerStyle = {
+    position: 'relative',
+    width: '1000px',
+    height: '800px',
+    ['font-family']: 'ms sans serif',
+    encapsulation: ViewEncapsulation.None
+  };
+
+
+  constructor(private venueService: VenueService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['vid'];
+          this.venue = this.venueService.getVenue(this.id);
+        }
+      );
+
+  }
+
+}
