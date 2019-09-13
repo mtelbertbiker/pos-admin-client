ff073381915578ba8ef7786e0124dd8e0fdf98be
Robert Johnson <mtelbertbiker@gmail.com>
2019-09-12 19:43:58 -0600

    Floorplans WIP

--- src/app/posconfig/floorplans/floorplan-list/floorplan-list.component.ts
+++ src/app/posconfig/floorplans/floorplan-list/floorplan-list.component.ts
@@ -3,6 +3,7 @@ import {Venue} from '../../../shared/pos-models/venue.model';
 import {VenueService} from '../../../venues/venue.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {SessionService} from '../../../shared/data-services/session.service';
+import {CdkDrag, CdkDragEnd} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-floorplan-list',
@@ -12,11 +13,14 @@ import {SessionService} from '../../../shared/data-services/session.service';
 export class FloorplanListComponent implements OnInit {
   venue: Venue;
   id: number;
+  initialPosition = { x: 100, y: 100 };
+  position = { ...this.initialPosition };
+  offset = { x: 0, y: 0 };
 
   constructor(private venueService: VenueService,
               private route: ActivatedRoute,
               private sessionService: SessionService,
-              private router: Router) { }
+              private router: Router) {}
 
   ngOnInit() {
     this.route.parent.params
@@ -26,6 +30,18 @@ export class FloorplanListComponent implements OnInit {
           this.venue = this.venueService.getVenue(this.id);
         }
       );
+  }
+
+  dragEnd(event: CdkDragEnd) {
+    this.offset = { ...(<any>event.source._dragRef)._passiveTransform };
+
+    this.position.x = this.initialPosition.x + this.offset.x;
+    this.position.y = this.initialPosition.y + this.offset.y;
+
+    console.log('New Position', this.position, this.initialPosition, this.offset);
+
+
+    // console.log(event);
   }
 
 }
---
+++ src/app/shared/pos-models/floorplan-item.model.ts
@@ -0,0 +1,11 @@
+export class FloorplanItem {
+  constructor(public LicId = 0,
+              public BId = 0,
+              public Lid = 0,
+              public FPId = 0,
+              public Id = 0,
+              public FloorPlanItemTypeId = 0,
+              public ItemId = 0,
+              public Position = { x: 0, y: 0 }
+  ) {}
+}
