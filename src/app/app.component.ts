3d42b4b4cb9f5b353a1e28844e14d82476925b5c
Robert Johnson <mtelbertbiker@gmail.com>
2020-09-29 06:39:32 -0600

    Update Android Min Requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {OidcSecurityService} from 'angular-auth-oidc-client';
-import {Router} from '@angular/router';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {UserFlow} from './shared/data-services/constants.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,32 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(public oidcSecurityService: OidcSecurityService, private router: Router) {}
+  constructor(public oidcSecurityService: OidcSecurityService, private router: Router) {
+  }
 
   ngOnInit() {
+
+    /*
+    this.router.events.subscribe(e => {
+//// LET's go straight to the point....
+      if (e instanceof NavigationStart) {
+          // HINT: this is the best place I've found to handle callbacks and redirections back to an Angular site,
+          // before even consider Auth stuff     ;)   So, in our case:
+          if (window.location.href.includes('AADB2C90118')) {
+            // We have to redirect the user to:
+            window.location.assign(
+              'https://feemachines.b2clogin.com/feemachines.onmicrosoft.com/oauth2/v2.0/authorize?'
+              + 'p=' + UserFlow.ResetPsw   // Your Reset policy (user flow) Name
+              + '&client_id=e0795570-377a-4064-8678-246db4734c21'  // Your client ID
+              + '&nonce=defaultNonce'
+              + '&redirect_uri=https://localhost:65328'   // ;)
+              + '&scope=openid' //
+              + '&response_type=id_token&prompt=login');
+          }
+      }
+    });
+*/
+
     this.oidcSecurityService
       .checkAuth()
       .subscribe((isAuthenticated) => {
@@ -26,7 +50,8 @@ export class AppComponent implements OnInit, OnDestroy {
       });
   }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+  }
 
   private navigateToStoredEndpoint() {
     const path = this.read('redirect');
